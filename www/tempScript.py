f545821d59eec0eed72a79dbe7367a22f32162db
Gogo Lejzr <ggljzr@localhost.localdomain>
2015-08-07 15:47:26 +0200

    refactoring

--- www/tempScript.py
+++ www/tempScript.py
@@ -13,20 +13,6 @@ client = BridgeClient()
 logBuffer = []
 logBufferMaxLen = 24
 
-def getDateFormat(timeStruct):
-	year = str(timeStruct[0])
-	month = str(timeStruct[1])
-	day = str(timeStruct[2])
-
-	return year + '-' + month + '-' + day
-
-def getTimeFormat(timeStruct):
-	hour = str(timeStruct[3]).zfill(2)
-	minute = str(timeStruct[4]).zfill(2)
-	sec = str(timeStruct[5]).zfill(2)
-
-	return hour + ':' + minute + ':' + sec
-
 def writeBufferToFile():
 	logFile = open('tempLog.log', 'w')
 	for entry in logBuffer:
@@ -39,7 +25,7 @@ while True:
 	#write into log every hour
 	if timeStruct[4] == 0:
 		val = client.get('temp')
-		currentTime = getTimeFormat(timeStruct)
+		currentTime = time.strftime('%H:%M%:%S', timeStruct)
 		entry = currentTime + ' ' + val
 		logBuffer.append(entry)
 		
